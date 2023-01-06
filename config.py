483995b1980a9e14349a0f00dad86135847409b8
Sainaangel <114768321+Sainaangel@users.noreply.github.com>
2023-01-06 17:56:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,11 +6,11 @@ class Development(Config):
     APP_ID = 10330886
     API_HASH = "8b88ae0d20e10c5b5c87f4d115fb6d7b"
     # the name to display in your alive message
-    ALIVE_NAME = "rishabh"
+    ALIVE_NAME = "┼⃖͢•🔥⃞⃪⃜🅐ниу┼⃖͢฿ØɎ⎯꯭̽𓆩🖤𓆪"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://vshklluv:4UcbbdhggBLKmAvjctEfKutpuv8PDcRO@john.db.elephantsql.com/vshklluv"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOHwBu4bOJkSN9MIDCcv7IqC160OIb-qpP65E3SiZOmx_IbESi3nmFSPMSzdHplRBET6utdRoEOwVGAH2ZkKqWbd51DflAhI9Y2UIcDU2Oo2V7jfqUm2Ez4UTc9ZrB7zsar2vMMhXywI5xdNKklJkGhkohdk-AOMUvrXK0AxODbmYF0PQNoraLNO_sP_-7L91KmEvDfAs4AnGRDxRuzZjxYuIpXJPCKLyWt9wks8dqsBJ61YGiC8mLz340CdGbh5NJUbYWoMdWcanaWV8CEwz9qJWkm4AjmA7kAUD1HMuqt45dLuzHvsW19n_uIflGhq-aPXWmF--imTUZ4n9X5F2cFX0EZA="
+    STRING_SESSION = "1BVtsOJIBuyMgpOJC79S0CoeY3HFLxt3FDtBS-BZi832saBZIK0CcwIdLUE4VPfJwoTXKJAr0ao1sIe5LXJQeFldYmfzFPMXwa0x_KGliGvVdoHX9srEv6zEFX6VtMxP7F6MfwhMQBoBaE6HWHuE1Cuqsyigy3EDFGZclWhv94K9O5HzFI8hCaQv_tg9_A4-diKDbgy678tVYwqu2urmpTB8q9DXUxLEd0UjClKnlqxtGzCHxVvNqlno1G5aEKYzsGGgmdXpb2R5-lu_EHmIolc8cqFCEofkEWY1a_o7n-od0L0hZcMMmnZN8PnL_nrxItL-gfCZKnMnwwkr0AuqGuUc6iN_6QDo="
     # create a new bot in @botfather and fill the following vales with bottoken
     TG_BOT_TOKEN = "5913548633:AAHjvZFeSDhL5IrPpd7lrIq-P3rTo_1y4oU"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
@@ -18,7 +18,7 @@ class Development(Config):
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "1AZWarzgBuzg3g4HJ3uFedm5TyXjR4tpsEWAd3YhJBRh6b1weDXyKW5WKMCMRqXi4M33y9LKP9C5KaPaQAgzOER0sGvHD4_L7UhdTAZac_qw2Udk48FitCGVskRV31isTeX4jIqALxrXc0ja-EwzmuykaWe019EOJzMZFjllHVmyHfOg5Kzxzq8y5n9_HkfZ1cGrwk-KhXlc01yerZFAJj64_qp5DpEc7z7SiLxZlwSlIxtyW0uNkwJ4deAQK0rlBIeBLYeyY_W98MzKM10Hec7f4tQxUNQW10jLGa3T9_EpGPkQtgvQORivBGJBY8FGwLzvy_Se18kALXf7IGNMzhmFBlJaf7Go=" #note don't use maim account to vc player
+    VC_SESSION = "1AZWarzcBuzN9CoMiHYFL7NX58biutPpgMcm9_O4RXk2UMZMh7-DOM9A7NUipjtnemm5q7RU5_G0y-VtLFqUWLaMhz-vbc7juI5Awu-vTlgq-8T1RuEGe1Jxbw78vka0P4IgaZ96W5n_4ZCIxxrnJMthA0B6JhoLb2c3AFsnIpGROc6IF5QB4k_xCChqE3Ohf4ryMGD0Hxuvjpqf6VYp1nY9AbEtnZah3ls1ANYg9mmpXDhImvH5jO9o6aKkC6d-d0I6_Wuc0MfqjmWxY_aKpxp1LIitGGZ8P3jjZ65kTJv-XnbgOZUNJjYbOxcJrpQGnIpsKVKW1qydemlaqXyvyFyN4GGYkiJo=" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
