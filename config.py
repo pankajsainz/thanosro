f16bece3e4d08f59443bc94b3784b79c2d82c5b7
Sainaangel <114768321+Sainaangel@users.noreply.github.com>
2023-01-20 18:39:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,25 +3,25 @@ from sample_config import Config
 
 class Development(Config):
     # get this values from the my.telegram.org
-    APP_ID = 10330886
-    API_HASH = "8b88ae0d20e10c5b5c87f4d115fb6d7b"
+    APP_ID = 6296490
+    API_HASH = "24385183c93a98ae4155c25d9f5f64b2"
     # the name to display in your alive message
-    ALIVE_NAME = "┼⃖͢•🔥⃞⃪⃜🅐ниу┼⃖͢฿ØɎ⎯꯭̽𓆩🖤𓆪"
+    ALIVE_NAME = "𝐈𝛕ᷟ‌𝚣⃪ꙴ ⋆‌⃝๛VENOM๛٭》[🇮🇳]"
     # create any PostgreSQL database (i recommend to use elephantsql) and paste that link here
     DB_URI = "postgres://vshklluv:4UcbbdhggBLKmAvjctEfKutpuv8PDcRO@john.db.elephantsql.com/vshklluv"
     # After cloning the repo and installing requirements do python3 stringsetup.py an fill that value with this
-    STRING_SESSION = "1BVtsOJIBuyMgpOJC79S0CoeY3HFLxt3FDtBS-BZi832saBZIK0CcwIdLUE4VPfJwoTXKJAr0ao1sIe5LXJQeFldYmfzFPMXwa0x_KGliGvVdoHX9srEv6zEFX6VtMxP7F6MfwhMQBoBaE6HWHuE1Cuqsyigy3EDFGZclWhv94K9O5HzFI8hCaQv_tg9_A4-diKDbgy678tVYwqu2urmpTB8q9DXUxLEd0UjClKnlqxtGzCHxVvNqlno1G5aEKYzsGGgmdXpb2R5-lu_EHmIolc8cqFCEofkEWY1a_o7n-od0L0hZcMMmnZN8PnL_nrxItL-gfCZKnMnwwkr0AuqGuUc6iN_6QDo="
+    STRING_SESSION = "1BJWap1wBu7m8vnEASysJcTNn4xYEcraND4bjwsIcVYFC0MgHRZV8DNrzdfZf2HVnuaph10GpH4jCR1GSoU26t_kKPVl5-PujlUnQCd5sJZjAs-FokY0XVmI-Wg0OL9OUfVZujZoavXFfZY8BncbL5CkddtRbdDjvuPrlIzvJ--B7xSW1nGoEZ34b_cC9ZAXT_IuZkmzKlVMHfft-TO-AhupNr-Az7tRBDUVtBC--a4aObbWV02wnBdPkgi2Ghx9Z7YF7D8QfNSKiRdjWaHMNXD6TWzipesG7ER9rbaBsM8KUJWCyo7LSkvIMh4J7iZLIZ06hHkEKQEC0pfCxF_JaPLaJLc1BJmc="
     # create a new bot in @botfather and fill the following vales with bottoken
-    TG_BOT_TOKEN = "5913548633:AAHjvZFeSDhL5IrPpd7lrIq-P3rTo_1y4oU"
+    TG_BOT_TOKEN = "5897701792:AAFIJ8NlfS5NwU3CrnO9RVsm7scvYs1j46k"
     # create a private group and a rose bot to it and type /id and paste that id here (replace that -100 with that group id)
-    PRIVATE_GROUP_BOT_API_ID = -1001885840624
+    PRIVATE_GROUP_BOT_API_ID = -100876251350
     # command handler
     COMMAND_HAND_LER = "."
     VCMODE = "True"
-    VC_SESSION = "1AZWarzcBuzN9CoMiHYFL7NX58biutPpgMcm9_O4RXk2UMZMh7-DOM9A7NUipjtnemm5q7RU5_G0y-VtLFqUWLaMhz-vbc7juI5Awu-vTlgq-8T1RuEGe1Jxbw78vka0P4IgaZ96W5n_4ZCIxxrnJMthA0B6JhoLb2c3AFsnIpGROc6IF5QB4k_xCChqE3Ohf4ryMGD0Hxuvjpqf6VYp1nY9AbEtnZah3ls1ANYg9mmpXDhImvH5jO9o6aKkC6d-d0I6_Wuc0MfqjmWxY_aKpxp1LIitGGZ8P3jjZ65kTJv-XnbgOZUNJjYbOxcJrpQGnIpsKVKW1qydemlaqXyvyFyN4GGYkiJo=" #note don't use maim account to vc player
+    VC_SESSION = "1AZWarzwBu29xUy21ahtVoAhYSu4ROBv-h5lpC7THSFGGUaSC0mKQMwIY79xi1UZi8u9DPgbnpbSsUjjTHyFXzf-ByGpeEtw9KF2b2np3Dm9vCRgITgX_-oBVxPQiaeBMrB6r5yAYrL_w-KEx9d-lvhNavV5FBVsbpKl71xB3QcPZXr-OPskTAEXlVUaJ71CtAPAu5Y30MprOSdjDgVpjOy6kMV42RQ2zM4QAYs3HBGXxVFh6XkgspisVXmtMv10gqNQ3Pq-KO7sSXWz-Bzbe9eEc-5mfQuQH4tMeRMXC7QK1hdcMJAT4Yd9kiQOUrfUzESOQTPeA9A8BwHPjIh_t8Y2il4EW8Zw=" #note don't use maim account to vc player
     # command hanler for sudo
     SUDO_COMMAND_HAND_LER = "."
     # External plugins repo
     EXTERNAL_REPO = "https://github.com/rishabhanand2/tha_plugins"
     #don't edit this 
-    THANOSABUSE = "False"#don't edit this else error in 
+    THANOSABUSE = "true"
